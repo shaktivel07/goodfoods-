809be85bb1034df0e7f5dc59b751ed420faac62a
Shaktivel07 <shaktivelkumaresan07@gmail.com>
2026-08-23 16:32:38 +0530

    ok

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -7,10 +7,10 @@ import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION!,
+  region: process.env.APP_AWS_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
